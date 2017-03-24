a171e9df02fecafd00b6f24671d5c4e1e1011c88
zhangzengmin <zhangzengmin@cydeng.com>
2017-03-24 16:46:57 +0800

    123123

--- startalimama.bat
+++
@@ -1,2 +0,0 @@
-java -jar F:\workspace_rocketmq\alimama2\alimama\target\alimama-0.0.1-SNAPSHOT.jar 
-
---
+++ startweiboread.bat
@@ -0,0 +1,2 @@
+java -jar F:\workspace_rocketmq\weiboread\target\weiboread-0.0.1-SNAPSHOT.jar 
+
